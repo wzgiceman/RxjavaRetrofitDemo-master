40d3336724c5aa0bf8498067d10dba2e9f41904b
wzgiceman <wzgiceman@gmail.com>
2017-03-28 15:50:45 +0800

    修改ID冲突问题

--- rxretrofitlibrary/src/main/java/com/wzgiceman/rxretrofitlibrary/retrofit_rx/Api/BaseApi.java
+++ rxretrofitlibrary/src/main/java/com/wzgiceman/rxretrofitlibrary/retrofit_rx/Api/BaseApi.java
@@ -26,7 +26,7 @@ public abstract class BaseApi<T> implements Func1<BaseResultEntity<T>, T> {
     /*是否需要缓存处理*/
     private boolean cache;
     /*基础url*/
-    private  String baseUrl="http://www.izaodao.com/Api/";
+    private  String baseUrl="https://www.izaodao.com/Api/";
     /*方法-如果需要缓存必须设置这个参数；不需要不用設置*/
     private String mothed;
     /*超时时间-默认6秒*/
